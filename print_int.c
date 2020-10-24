edbeb28f62fcb22ac99b5d9deee70336c117ce57
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-10-24 21:53:36 +0000

    script contaning the function to print integers

--- print_int.c
+++ print_int.c
@@ -1,11 +1,33 @@
 #include "holberton.h"
 /**
+ *print_rec - is called by print_int function to print out numbers to the SO
+ *@num: The number to be output
+ */
+void print_rec(int num)
+{
+	if (num > 10)
+	{
+		print_rec(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
+/**
  *print_int - this function prints a number to the standard output
- *@str: The number to be output
+ *@num: The number to be output
  *Return: returns the amount of characters printed from the function
  */
 int print_int(int num)
 {
-	/*keeps track of how many characters are being printed*/
-	int num_char = 0;
+	int count = 1;
+
+	if (num < 0)
+		_putchar('-');
+
+	print_rec(num);
+	while (num > 10)
+	{
+		num /= 10;
+		count++;
+	}
+	return (count);
 }
