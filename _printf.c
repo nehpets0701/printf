bfa334145e5f796ac849122daa6af0dc34d774c7
nehpets <nehpets0701@gmail.com>
2020-10-24 21:41:40 +0000

    building structrue for main _printf function

--- _printf.c
+++ _printf.c
@@ -6,5 +6,85 @@
  */
 int _printf(const char *format, ...)
 {
+	int i = 0;
+	int char_count = 0;
+	va_list cases;
 
+	va_start(cases, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case '%':
+			char_count += casePercent(format[i + 1]);
+		case '\\':
+			char_count += caseSlash(format[i + 1]);
+		}
+		_putchar(format[i]);
+		char_count++;
+	}
+	return (char_count);
+}
+
+/**
+ *caseSlash-handles the character after a '\'
+ *Return:number of characters printed
+ *@c:character after '\'
+ */
+int caseSlash(char c)
+{
+	int char_count = 0;
+
+	switch (c)
+	{
+	case '\\':
+		_putchar('\\');
+		i++;
+		char_count++;
+		break;
+	case 'n':
+		_putchar('\n');
+		i++;
+		char_count++;
+		break;
+	case 't':
+		_putchar('\t');
+		i++;
+		char_count++;
+		break;
+	}
+	return (char_count);
+}
+
+/**
+ *casePercent-handles the character after a '%'
+ *Return:number of characters printed
+ *@c:character after '%'
+ */
+int casePercent(char c)
+{
+	int char_count = 0;
+
+	switch (c)
+	{
+	case 'c':
+		_putchar(va_arg(cases, char));
+		i++;
+		char_count++;
+		break;
+	case 's':
+		char_count += print_string(va_arg(cases, char*));
+		i++;
+		break;
+	case 'd': case 'i':
+		char_count += print_int(va_arg(cases, int));
+		i++;
+		break;
+	case '%':
+		_putchar('%');
+		i++;
+		char_count++;
+		break;
+	}
+	return (char_count);
 }
