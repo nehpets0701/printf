e454f32f7cd2eb80b8b5d86c96e117f8509bcc11
nehpets <nehpets0701@gmail.com>
2020-10-25 16:01:45 +0000

    pushing hex function skeleton to master

---
+++ print_hex.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *print_hex-prints unsigned decimal value as hexadecimal
+ *Return: the number of characters printed
+ *@num:unsigned decimal input
+ */
+int print_hex(unsigned int num)
+{
+	int char_count = 0;
+
+	return (char_count);
+}
